2f786b1bc84e90c0abd62261e3fa48ac2136d24a
André Sheydin <andre@sheydin.de>
2026-05-05 19:57:40 +0200

    sync: monorepo 2026-05-05 (validator 0.1.3, validation-types 0.1.1)

--- packages/validator/src/core/__tests__/logger-helper.ts
+++
@@ -1,5 +0,0 @@
-export const log = {
-  info: (...args: unknown[]) => console.info(...args),
-  warn: (...args: unknown[]) => console.warn(...args),
-  error: (...args: unknown[]) => console.error(...args),
-};
